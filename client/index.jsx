d25bf3c552a7fd1c42df28edaef6e96dad064f12
Kristoffer Habberstad <kristoffer.habberstad@gmail.com>
2022-03-18 15:40:35 +0100

    Init commit

---
+++ client/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+function Frontpage() {
+  return (
+    <div>
+      <h1>Frontpage</h1>
+      <div>
+        <div>
+          <Link to={"login"}>Login</Link>
+        </div>
+        <div>
+          <Link to={"profile"}>Profile</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Frontpage />} />
+        <Route path={"/login"} element={"Login"} />
+        <Route path={"/login/callback"} element={"Login callback"} />
+        <Route path={"/profile"} element={"Profile"} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
